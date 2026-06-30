a623b35157a65caef29f4cbc1d585ac4977de39f
VladimirKataev <vova.kataev@gmail.com>
2026-06-29 20:14:23 -0700

    Service worker can intercept fetch, activate, install

--- docs/app.js
+++ docs/app.js
@@ -4,6 +4,65 @@ const fuelInputEl = document.getElementById("fuelConsumed");
 const distanceInputEl = document.getElementById("distanceTravelled");
 const STORAGE_KEY = "mileageTracker";
 const pastTripsContainer = document.getElementById("pastTrips");
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icon-512.png",
+];
+const VERSION = "v0.1.0";
+
+const CACHE_NAME = `mileageTracker-${VERSION}`;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // when seeking an HTML page
+  if (event.request.mode === "navigate") {
+    // Return to the index.html page
+    event.respondWith(caches.match("./"));
+    return;
+  }
+
+  // For every other request type
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // Respond with a HTTP 404 response status.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
 
 // Listen to form submissions. On each submit, new event happens
 newTripFormEl.addEventListener("submit", (event) => {
